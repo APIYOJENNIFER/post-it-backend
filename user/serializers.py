eefaf6413e26ad7c415b6b76904b0a96f7bdeb1e
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-10-24 15:27:47 +0300

    Hash user password

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth.hashers import make_password 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.password = make_password(password)
+        user.save()
+        return user
