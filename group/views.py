f3cb384343e70d91fce07d054c5ad62c31f2fc78
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-10-30 15:13:13 +0300

    Create broadcast groups

--- group/views.py
+++ group/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import GroupSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_group(request):
+    if request.method == 'POST':
+        data = request.data.copy()
+        data['creator']=request.user.id
+        serializer = GroupSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
