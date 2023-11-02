cc0ff9394d1f33a93dd86c377c8dc7da3b818cf6
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-11-02 12:26:53 +0300

    Create api to add users to a group

--- group/urls.py
+++ group/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_group)
+    path('', views.create_group),
+    path('<int:group_id>/user/', views.add_users),
 ]
