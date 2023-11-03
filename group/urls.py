5d9d066ea9cf11f62144918fdad0b532ccf4f9ed
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-11-03 12:31:18 +0300

    Create API to post messages to group

--- group/urls.py
+++ group/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.create_group),
     path('<int:group_id>/user/', views.add_users),
+    path('<int:group_id>/message/', views.post_message),
 ]
--- group/views.py
+++ group/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Group
-from .serializers import GroupSerializer
+from .serializers import GroupSerializer, MessageSerializer
 
 # Create your views here.
 
@@ -48,3 +48,31 @@ def add_users(request, group_id):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def post_message(request, group_id):
+    """Post message to a group"""
+    if request.method == 'POST':
+        try:
+            group = Group.objects.get(id=group_id)
+            user = request.user
+        except Group.DoesNotExist:
+            return Response({"error":"Group not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if user in group.members.all():
+            data = {
+                "post": request.data.get("post"),
+                "group": group.id,
+                "user":user.id,
+            }
+        
+            serializer = MessageSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error":"User is not a member of this group"}, status=status.HTTP_403_FORBIDDEN)
