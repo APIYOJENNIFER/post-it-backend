f48a7b3e52c5dafb0042ce74222c76b001761508
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-02-27 14:42:16 +0300

    Modify resource to use plural form

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('user/', include('user.urls')),
-    path('group/', include('group.urls')),
+    path('users/', include('user.urls')),
+    path('groups/', include('group.urls')),
 ]
