bf05325de894d6319c84945744fbb1906152fd4b
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-03-05 01:04:44 +0300

    Separate test for signup and signin

--- user/tests/test_users_api.py
+++ user/tests/test_users_api.py
@@ -1,32 +1,52 @@
 import pytest
 from django.urls import reverse
 from django.contrib.auth.models import User
+from rest_framework import status
+
+@pytest.fixture
+def create_user(db):
+    """
+    Create a user object
+    :param db
+    :return None
+    """
+    def make_user(username, password, email):
+        User.objects.create_user(username=username, password=password, email=email)
+    return make_user
 
 @pytest.mark.django_db
 def test_user_signup_api(client):
     """
-    Test the user signup and signin APIs
+    Test the user signup API
     :param client
     :return None
     """
 
     url = reverse('signup')
-    payload = {
+    data = {
         "username":"testuser",
         "password":"123",
         "email":"testuser@gmail.com"
     }
-    response = client.post(url, data=payload, format="json")
-    assert response.status_code == 201
+    response = client.post(url, data=data, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
 
     user = User.objects.get(username="testuser")
     assert user.username == "testuser"
 
-    #user signin
-    url_signin = reverse('signin')
-    data_signin = {
+@pytest.mark.django_db
+def test_user_signin_api(client, create_user):
+    """
+    Test the user signin API
+    :param client, create_user
+    :return None
+    """
+    create_user("testuser", "123", "testuser@gmail.com")
+    url = reverse('signin')
+    data = {
         "username":"testuser",
         "password":"123"
     }
-    response_signin = client.post(url_signin, data=data_signin, format="json")
-    assert response_signin.status_code == 200
+
+    response = client.post(url, data=data, format="json")
+    assert response.status_code == status.HTTP_200_OK
