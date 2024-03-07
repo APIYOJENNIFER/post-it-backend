5f4d32949db78a6856597016473b7a80edc86f9a
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-03-07 12:51:24 +0300

    Create test user and group

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 from django.urls import reverse
+from group.models import Group
 
 
 @pytest.fixture(name="api_client")
@@ -21,8 +22,8 @@ def fixture_create_user(db):
     :return None
     """
     def make_user(username, password, email):
-        User.objects.create_user(username=username,
-                                 password=password, email=email)
+        return User.objects.create_user(username=username,
+                                        password=password, email=email)
     return make_user
 
 
@@ -32,17 +33,43 @@ def authenticate_user_with_token(db, api_client, create_user):
     Authenticate user with token after signin
     :params db, api_client, create_user
     """
-    def authenticate_user():
+    def authenticate_user(username, password, email):
         """
         Authenticate user
         """
-        create_user("testuser", "123", "testuser@gmail.com")
+        if not User.objects.filter(username=username).exists():
+            create_user(username, password, email)
         url = reverse('signin')
         data = {
-            "username": "testuser",
-            "password": "123"
+            "username": username,
+            "password": password
             }
 
         response = api_client.post(url, data=data, format="json")
+        print(response.data)
         return response.data["token"]
     return authenticate_user
+
+
+@pytest.fixture(name="group_creator")
+def fixture_group_creator(db, create_user):
+    """Create a user who is a group creator to test group delete"""
+
+    return create_user(username='test_creator',
+                       password='123', email='testcreator@gmail.com')
+
+
+@pytest.fixture(name="another_user")
+def fixture_another_user(db, create_user):
+    """Create a user object who isn't a group creator to test group delete"""
+
+    return create_user(username='not_creator',
+                       password='123', email='notcreator@gmail.com')
+
+
+@pytest.fixture
+def test_group(db, group_creator):
+    """Create a test group"""
+    group = Group.objects.create(name="Test group", creator=group_creator)
+    group.members.set([group_creator])
+    return group
