5699b2c93185a3613a3c08b57a634035fecef1de
APIYOJENNIFER <apiyojennifer4@gmail.com>
2024-03-12 18:33:16 +0300

    Add API documentation using swagger

--- post_it_backend/urls.py
+++ post_it_backend/urls.py
@@ -16,8 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='POST IT APIs',
+        default_version='v1',
+        description='API documentation',
+        terms_of_service='',
+        contact=openapi.Contact(email=''),
+        license=openapi.License(name='BSD Licence'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    #DRF URLs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
